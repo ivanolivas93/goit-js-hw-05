7928538678b87ab6f858dd34e4cd0e205d9b931d
Juan Olivas <juanolivas0693@gmail.com>
2020-08-04 13:30:46 +0300

    module-05

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    storage.removeItem(item);
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
+
+const items = storage.getItems();
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
